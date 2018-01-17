556aeabf95d130e26546056edd070f2e6606e27c
Poornima <>
2018-01-17 18:29:09 +0530

    first commit

---
+++ MA01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MA01
+//
+//  Created by Poornima on 17/01/18.
+//  Copyright © 2018 Poornima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MA01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MA01
+//
+//  Created by Poornima on 17/01/18.
+//  Copyright © 2018 Poornima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
